ab913df0449981378a2b4c7a67e56a6da0434682
fatiha <fathiato@gmail.com>
2023-07-13 23:52:37 +0100

    processing data

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,11 @@
 class Like < ApplicationRecord
-  belongs_to :author
+  belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  after_save :update_like_counter
+  def update_like_counter
+    post = Post.find_by_id(post_id)
+    post.likes_counter = post.likes_counter.to_i + 1
+    post.save
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,16 @@
 class Post < ApplicationRecord
-  belongs_to :author
+  belongs_to :author, class_name: 'User'
+  has_many :comments
+  has_many :likes
+
+  after_save :update_post_counter
+  def update_post_counter
+    postr = User.find_by_id(author.id)
+    postr.posts_counter = poster.posts_counter.to_i + 1
+    postr.save
+  end
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
