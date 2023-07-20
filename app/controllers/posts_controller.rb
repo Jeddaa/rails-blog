722d459c4833c2077b3dbfd16e4f54b53e774ff8
fatiha <fathiato@gmail.com>
2023-07-20 02:04:14 +0100

    creating views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,23 @@
 class PostsController < ApplicationController
-  def show; end
+  before_action :index
+  def index
+    # @posts = Post.where(author_id: params[:user_id])
+    # # @user = User.find(id:params[:user_id])ß
+    @user = User.find(params[:user_id])
+    @posts = @user.posts.includes(:comments, comments: :author).order(created_at: :desc)
+  end
 
-  def index; end
+  def show
+    @post = Post.find_by(id:params[:id])
+  end
+
+  # private
+
+  # def set_user
+  #   @user = User.find(params[:user_id])
+  # end
+
+  # def get_comments
+  #   @comments = Comment.where(post_id: params[:id])
+  # end
 end
