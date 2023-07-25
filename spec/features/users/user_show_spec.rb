97ec18fd1d6d28af2fcf7f212cfccf0d0e428f98
mnaibei <muchajulius@gmail.com>
2023-07-25 11:30:36 +0300

    Added User show spec

--- spec/features/users/user_show_spec.rb
+++ spec/features/users/user_show_spec.rb
@@ -24,5 +24,36 @@ RSpec.describe 'User', type: :feature do
                          author: @user1)
   end
 
+  describe 'user#show' do
+    before(:each) do
+      visit user_path(@user1.id)
+    end
+    it 'should show the username of the user' do
+      expect(page).to have_content(@user1.name)
+    end
+    it 'should show the profile picture for the user.' do
+      expect(page).to have_xpath("//img[contains(@src,'https://google.com')]")
+    end
+    it 'should show the number of posts the user has written.' do
+      expect(page).to have_content('Number of posts: 4')
+    end
+    it 'should show the bio of the user' do
+      expect(page).to have_content(@user1.bio)
+    end
+    it 'I can see the user first 3 posts.' do
+      expect(page).to_not have_content('Total number of posts: 4')
+    end
+    it 'should see a button that lets me view all of a user\'s posts' do
+      expect(page).to have_content('See all posts')
+    end
+    it 'should redirect to the post\'s show page when user post is clicked.' do
+      visit "/users/#{@user1.id}/posts/#{@post1.id}"
+      expect(page).to have_content('My first rejection')
+    end
+    it 'should redirect to user1\'s show page when user is clicked.' do
+      click_link 'See all posts'
+      expect(page).to have_current_path('/users')
+    end
+  end
 
 end
